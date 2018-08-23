f0b3b0af0e4d6bc46de3a3f47eeec966b294a9a3
Wojciech Surdyk <ajdutt13@gmail.com>
2018-08-23 10:11:27 +0200

    added VS solution

---
+++ Core/InputStream.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Core
+{
+   public class InputStream
+   {
+      private string _file = string.Empty;
+
+      private int _pos = 0,
+                  _col = 0, _ln = 1;
+
+      public InputStream(string path)
+      {
+         if (File.Exists(path))
+            _file = File.ReadAllText(path);
+         else throw new FileNotFoundException("Could not find a file in path: " + path);
+      }
+
+      public char? Peek()
+      {
+         if (Eof( ))
+            return null;
+         return _file[_pos];
+      }
+
+      public char? Next()
+      {
+         var c = Peek( );
+         ++_pos;
+         return c;
+      }
+
+      public bool Eof( )
+         => _pos >= _file.Length;
+
+      public Exception Croak(string msg)
+      {
+         return new Exception($"{msg} ({_col}:{_ln}");
+      }
+   }
+}
---
+++ Core/Tokenizer.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Core
+{
+   using Token = Dictionary<string, object>;
+
+   public class Tokenizer
+   {
+      private Token _curr = new Token( );
+      private InputStream _input = null;
+
+      private const string _kws =
+         " if else char int long float double string def namesp class true false ";
+
+      private bool IsKw(string x)
+         => _kws.IndexOf($" {x} ") >= 0;
+
+      private bool IsDigit(string c)
+         => Regex.IsMatch(c, "[0-9]");
+
+      private bool IsIdStart(string c)
+         => Regex.IsMatch(c, "[a-bA-B_]");
+
+      private bool IsId(string c)
+         => Regex.IsMatch(c, "[a-bA-B0-9_]");
+
+      private bool IsOper(string c)
+         => Regex.IsMatch(c, "[+-*/%=&|<>!]");
+
+      private bool IsPunc(string c)
+         => Regex.IsMatch(c, @"[,.;(){}\[\]]");
+
+      private bool IsWs(string c)
+         => Regex.IsMatch(c, "[ \t\n]");
+
+      private string RdWhile(Func<string, bool> pred)
+      {
+         string s = string.Empty;
+         while (!_input.Eof( ) && pred(_input.Peek( ).ToString( )))
+         {
+            s += _input.Next( );
+         }
+         return s;
+      }
+
+      private Token RdNum()
+      {
+         bool dot = false;
+         Func<string, bool> f = (c) =>
+         {
+            if (c == ".")
+            {
+               if (dot)
+                  return false;
+               dot = true;
+               return true;
+            }
+            return IsDigit(c);
+         };
+         string num = RdWhile(f);
+
+         var ret = new Token( );
+         ret["type"] = "num";
+         ret["value"] = num;
+
+         return ret;
+      }
+
+      private Token RdId()
+      {
+         var id = RdWhile(IsId);
+
+         var ret = new Token( );
+         ret["type"] = IsKw(id) ? "kw" : "var";
+         ret["value"] = id;
+
+         return ret;
+      }
+
+
+   }
+}
