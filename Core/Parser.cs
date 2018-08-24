71c681d0c63181c5664719f656446460b9867125
Wojciech Surdyk <ajdutt13@gmail.com>
2018-08-24 09:56:28 +0200

    Started making the parser

---
+++ Core/Parser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+   /// <summary>
+   /// Parses the tokens made by the <see cref="Tokenizer"/>
+   /// </summary>
+   public class Parser
+   {
+      /// <summary>
+      /// Default constructor
+      /// </summary>
+      public Parser( )
+      { }
+
+      
+   }
+}
