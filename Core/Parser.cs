40867dd287406755a158c590013b061328a5294f
Wojciech Surdyk <ajdutt13@gmail.com>
2018-08-25 18:55:16 +0200

    Added input

--- Core/Parser.cs
+++ Core/Parser.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Core
+﻿namespace Core
 {
    /// <summary>
    /// Parses the tokens made by the <see cref="Tokenizer"/>
    /// </summary>
    public class Parser
    {
+      private Tokenizer _input = null;
+
       /// <summary>
       /// Default constructor
       /// </summary>
       public Parser( )
       { }
+
+      public Parser(string path)
+         => _input = new Tokenizer(path);
 
       
    }
